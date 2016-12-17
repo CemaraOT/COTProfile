6012a6e0aab58130712d64e7336d5d7343763aba
dhifaizhar <mdhifaizhar@gmail.com>
2016-12-18 05:59:07 +0700

    All View Page - New

--- application/views/main.php
+++ application/views/main.php
@@ -2,65 +2,55 @@
     <title>COT | We're hearing</title>
 </head>
 <?php $this->load->view("common/header");?>
-<div class="clearfix"></div>
 <div class="content-page">
+    <br/>
     <div class="jumbotron-v2 text-center">
-		<br/>
-        <h1>COT | We're hearing</h1>
-        <h3>Layanan pembuatan aplikasi terbaik dengan biaya rendah</h3><br/>
-        <br/>
-        <br/>
-		<!--
-        <div class="row text-center">
-            <div class="col-lg-4 hidden-xs"></div>
-            <div class="col-lg-2 col-xs-6">
-                <img class="img-circle" src="<?php echo base_url();?>assets/img/service.png" alt="Service Center" width="100" height="100">
+        <div class="content">
+            <img src="<?php echo base_url()?>/assets/img/ico_COT_lg.png" class="img-circle" style="width: 100px;height: 100px;"/>
+            <h1>COTech</h1>
+            <h3>Design & Development Website</h3>
+            <hr class="featurette-heading"/>
+            <br/>
+            <div class="row">
+                <span class="col-sm-4">
+                    <a href="www.facebook.com" title="facebook">
+                        <img class="img" src="<?php echo base_url();?>assets/img/fb.png" alt="Facebook" width="30" height="30">
+                    </a>
+                </span>
+                <span class="col-sm-4">
+                    <a href="www.twitter.com" title="twitter">
+                        <img class="img" src="<?php echo base_url();?>assets/img/tw.png" alt="Twitter" width="30" height="30">
+                    </a>
+                </span>
+                <span class="col-sm-4">
+                    <a href="plus.google.com" title="google plus">
+                        <img class="img" src="<?php echo base_url();?>assets/img/gp.png" alt="Google Plus" width="30" height="30">
+                    </a>
+                </span>
             </div>
-            <div class="col-lg-2 col-xs-6">
-                <img class="img-circle" src="<?php echo base_url();?>assets/img/partner.png" alt="Trusted Distributors" width="100" height="100">
-            </div>
-            <div class="col-lg-2 col-xs-6">
-                <img class="img-circle" src="<?php echo base_url();?>assets/img/aktifasi.png" alt="Aktifasi Garansi" width="100" height="100">
-            </div>
-            <div class="col-lg-2 hidden-xs"></div>
         </div>
-		-->
     </div>
     <div class="contain-page">
         <div class="container">
-            <hr class="featurette-divider"/>
-            <h1 class="text-center text-bold">Layanan Kami</h1>
-            <hr class="featurette-divider"/>
+            <h1 class="text-center text-bold">Our Services</h1><br/>
             <div class="row featurette" >
-                <div class="col-md-1 text-right"></div>
-                <div class="col-md-5 text-center">
-                    <img class="featurette-image img-circle center-block" src="<?php echo base_url();?>assets/img/partner.png" alt="product partner" width="100" height="100" />
-                    <h3 class="featurette-heading">CO Partner | <span class="text-muted">Mitra Kami.</span></h3>
-                    <p>Kami bekerja bersama mitra terpercaya untuk memperoleh kepercayaan.</p>
+                <div class="col-md-4 text-center">
+                    <img class="featurette-image img-circle center-block" src="<?php echo base_url();?>assets/img/aktifasi.png" alt="product partner" width="75" height="75" />
+                    <h3 class="featurette-heading">Design | <span class="text-muted">Web Design</span></h3>
+                    <p>We design a website, its all your taste.</p>
                 </div>
-                <div class="col-md-5 text-center">
-                    <img class="featurette-image img-circle center-block" src="<?php echo base_url();?>assets/img/service.png" alt="service center" width="100" height="100" />
-                    <h3 class="featurette-heading">CO Service | <span class="text-muted">Dukungan Perbaikan.</span></h3>
-                    <p>Kami akan selalu menjadi mitra penanganan aplikasi yang secara kontinu bekerja sama dengan untuk menjaga kualitas.</p>
+                <div class="col-md-4 text-center">
+                    <img class="featurette-image img-circle center-block" src="<?php echo base_url();?>assets/img/aktifasi.png" alt="distributors" width="75" height="75" />
+                    <h3 class="featurette-heading">Application | <span class="text-muted">Website Apps</span></h3>
+                    <p>Support All Your Bussiness with Powerfull Website Application.</p>
                 </div>
-                <div class="col-md-1 text-left"></div>
+                <div class="col-md-4 text-center">
+                    <img class="featurette-image img-circle center-block" src="<?php echo base_url();?>assets/img/aktifasi.png" alt="aktifasi" width="75" height="75" />
+                    <h3 class="featurette-heading">Application | <span class="text-muted">Mobile Apps</span></h3>
+                    <p>Our service come soon.</p>
+                </div>
             </div>
-            <br/><br/>
-            <div class="row featurette">
-                <div class="col-md-1 text-right"></div>
-                <div class="col-md-5 text-center">
-                    <img class="featurette-image img-circle center-block" src="<?php echo base_url();?>assets/img/aktifasi.png" alt="distributors" width="100" height="100" />
-                    <h3 class="featurette-heading">CO Trusted | <span class="text-muted">Produk Terbaik.</span></h3>
-                    <p>Kami hanya membuat produk terbaik meski dengan modal yang kecil. <i>Small but be Powerfull</i>.</p>
-                </div>
-                <div class="col-md-5 text-center">
-                    <img class="featurette-image img-circle center-block" src="<?php echo base_url();?>assets/img/aktifasi.png" alt="aktifasi" width="100" height="100" />
-                    <h3 class="featurette-heading">CO Guarantee | <span class="text-muted">Garansi.</span></h3>
-                    <p>Kami selalu menjamin produk kami berjalan sesuai dengan kabutuhan perusahaan.</p>
-                </div>
-                <div class="col-md-1 text-left"></div>
-            </div>
-            <br/><br/>
+            <br/>
         </div>
     </div>
     <div class="submenu">
